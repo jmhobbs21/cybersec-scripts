db163390fe52cf5e261e12167afdb9c65c896552
jmhobbs21 <86446667+jmhobbs21@users.noreply.github.com>
2022-05-23 15:26:55 -0400

    Update urlscan.py

--- urlscan.py
+++ urlscan.py
@@ -2,14 +2,15 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+#Python script to run urls through urlscan.io
 
 load_dotenv()
 
-api_key=""
+api_key="" #api key pulled from environment
 
 url = input("Paste the url\n")
 
-headers = {'API-Key': os.getenv("url_scan_api_key"),'Content-Type':'application/json'}
+headers = {'API-Key': os.getenv("your_api_key"),'Content-Type':'application/json'} #header for pulling api key from environment
 data = {"url": f'{url}', "visibility": "public"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
